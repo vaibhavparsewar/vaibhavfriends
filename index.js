d138c2649e04851a538d0e0c8a4462f8bde86314
vaibhav parsewar <82768659+vaibhavparsewar@users.noreply.github.com>
2024-01-11 00:55:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const yesBtn = document.querySelector('.yes-btn');
 const noBtn = document.querySelector('.no-btn');
 const wrapperRect = wrapper.getBoundingClientRect();
 const noBtnRect = noBtn.getBoundingClientRect();
-yesBtn.addEventListener('click', () => {
-    question.innerHTML = 'message me on insta then :)';
-});
+
 noBtn.addEventListener('mouseover', () => {
     const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
     const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
     noBtn.style.left = i + 'px';
     noBtn.style.top = j + 'px';
+});
+
+yesBtn.addEventListener('click', () => {
+    question.innerHTML = 'message me on insta then :)';
 });
